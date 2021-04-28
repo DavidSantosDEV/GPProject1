cfa4947601f4a8bfbadfdc82b1a0cf1107bfed84
SSa1nt <39455909+SSa1nt@users.noreply.github.com>
2021-04-28 16:44:26 +0100

    biter

--- GP_Project1/Assets/Scripts/Biter/BiterBehaviour.cs
+++ GP_Project1/Assets/Scripts/Biter/BiterBehaviour.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class BiterBehaviour : MonoBehaviour
 {
+    EnemyAnimation myAnimationMaster;
 
-    
+    //I kinda wanna do this this way but kinda don't
+    HealthSystem hurtThisGuy;
+
+    [SerializeField]
+    private float biteDamage = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnimationMaster = GetComponent<EnemyAnimation>();
     }
 
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isActiveAndEnabled)
+        {
+            if (collision.CompareTag("Enemy"))
+            {
+                hurtThisGuy = null;
+                hurtThisGuy = collision.GetComponent<HealthSystem>();
+                if(hurtThisGuy)
+                EnactBite();
+            }
+        }
+    }
+
+    private void EnactBite()
+    {
+        myAnimationMaster.PlayAttackAnimation();
+    }
+
+    public void DoBiteEffect()
+    {
+        hurtThisGuy.TakeDamage(biteDamage);
+    }
 }
